9aefbe89d2c41d25a24e5fdc8b93613d84728167
Fredrik Valdt <valdt@kth.se>
2022-07-28 22:01:16 +0200

    Meow

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,10 @@ import Hero from "../components/home-page/hero";
 
 const DUMMY_POSTS= [
   {title:'Nap',image:'cat300.jpg',excerpt:'The worst nap, not clickbiat. Owner in shock',date:'2022-02-10',slug:'nap'},
-  {title:'Nap',image:'cat300.jpg',excerpt:'The worst nap, not clickbiat. Owner in shock',date:'2022-02-10',slug:'murder'},
-  {title:'Nap',image:'cat300.jpg',excerpt:'The worst nap, not clickbiat. Owner in shock',date:'2022-02-10',slug:'catnip-found'},
-  {title:'Nap',image:'cat300.jpg',excerpt:'The worst nap, not clickbiat. Owner in shock',date:'2022-02-10',slug:'rehab'},
-  {title:'Nap',image:'cat300.jpg',excerpt:'The worst nap, not clickbiat. Owner in shock',date:'2022-02-10',slug:'genocide-northafrica'},
+  {title:'Nap',image:'cat300.jpg',excerpt:'Unidentified mouse found outside residential area.',date:'2022-02-10',slug:'murder'},
+  {title:'Nap',image:'cat300.jpg',excerpt:'The worst drug epedemic since 310bc',date:'2022-02-10',slug:'catnip-found'},
+  {title:'Nap',image:'cat300.jpg',excerpt:'Childhood star actor out of rehab, meow her first!',date:'2022-02-10',slug:'rehab'},
+  {title:'Nap',image:'cat300.jpg',excerpt:'Shower ends life of many dreams for workingclass cats.',date:'2022-02-10',slug:'genocide-northafrica'},
   {title:'Nap',image:'cat300.jpg',excerpt:'The worst nap, not clickbiat. Owner in shock',date:'2022-02-10',slug:'are-we-gods'},
 ]
 
